475c7f0ad53a3bac8c09e80d2f57bd03919b5b05
YoruanOrta <yoruanorta50@gmail.com>
2025-03-04 13:43:18 -0400

    User file update

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -2,8 +2,10 @@ import re
 from sqlalchemy import Column, String, Boolean
 from sqlalchemy.orm import relationship, validates
 from app.models.base_model import BaseModel
+""" User module """
 
 class User(BaseModel):
+    """ User class """
     email = Column(String(128), nullable=False, unique=True)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
@@ -12,6 +14,7 @@ class User(BaseModel):
     reviews = relationship('Review', backref='user', cascade='all, delete')
 
     def __init__(self, email, first_name=None, last_name=None, is_admin=False):
+        """ Constructor """
         super().__init__()
         if not self.validate_email(email):
             raise ValueError("Invalid email format")
@@ -26,13 +29,13 @@ class User(BaseModel):
 
     @staticmethod
     def validate_email(email):
-        """Valida que el email tenga un formato correcto"""
+        """Validate that the email has a correct format"""
         email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
         return re.match(email_regex, email) is not None
 
     @validates('first_name', 'last_name')
     def validate_names(self, key, value):
-        """Valida que first_name y last_name no sean cadenas vacías"""
+        """Validate that first_name and last_name are not empty strings"""
         if value and value.strip() == "":
             raise ValueError(f"{key} cannot be empty")
         return value
