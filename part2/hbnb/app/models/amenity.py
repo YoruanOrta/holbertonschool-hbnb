a447c8b50f6deae828739861d1e018b188671a7d
YoruanOrta <yoruanorta50@gmail.com>
2025-03-04 13:36:11 -0400

    Amenity class and module update

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -2,8 +2,10 @@ from app.models.base_model import BaseModel
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
+""" Amenity Module for the HBNB project """
 
 class Amenity(BaseModel):
+    """ Amenity class to store information about an Amenity """
     name = Column(String(128), nullable=False)
     place_amenities = relationship('Place', secondary='place_amenity', back_populates="amenities", lazy="joined")
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
