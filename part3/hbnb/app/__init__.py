cc8886827d20b91ca379cc2dbbe758e3ec1012e4
YoruanOrta <yoruanorta50@gmail.com>
2025-03-10 13:39:16 -0400

    refactor create_app function to accept configuration class and improve API setup

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,17 +2,21 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api import create_api
-""" Create a Flask app and configure it with the API """
 
-def create_app(testing=False):
-    """ Create a Flask app and configure it with the API """
+def create_app(config_class=None):
+    """Create a Flask application and configure it with the API"""
     app = Flask(__name__)
+
+    # Use the provided configuration or a default one
+    if config_class:
+        app.config.from_object(config_class)
+
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    if testing:
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-
+    # Register the API namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
+
+    # Register other endpoints or API configurations
     create_api(app)
+
     return app
